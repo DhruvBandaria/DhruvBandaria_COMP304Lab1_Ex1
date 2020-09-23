ec60ac59be6cfcf53b6644ba4793665d84c953bf
Dhruv <dhruvbandaria@hotmail.com>
2020-09-23 14:24:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dhruvbandaria_comp304lab1_ex1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.dhruvbandaria_comp304lab1_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.textView6);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
